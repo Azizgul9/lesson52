8991413bd8b744bc7190a426e8b318956a282bb9
Azizgul <azizgulorozalieva6@gmail.com>
2019-04-16 10:24:43 +0600

    Created function that has a props.Props gets the values from parent state.Stylised our component

---
+++ src/components/Rendomizenum/Rendomizenum.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Rendomizenum.css';
+
+const Rendomizenum = (props) => {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        <li className={"num_item"}>{number}</li>
+    );
+    return (
+        <ul className={"num_list"}>{listItems}</ul>
+    );
+};
+
+export default Rendomizenum;
